250bbd2fc63920b633fe7825f401359ea0b9ef7e
veeravignesh1 <kveeravignesh1@gmail.com>
2020-11-17 21:51:17 +0530

    adding templates without extend

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask,render_template,url_for,request
 
-app = Flask("__name__")
+posts =[
+    {
+        'author':'Veera',
+        'title': "First Blog Post",
+        'content': "Content of first blog post",
+        'date posted': "11-Nov-2020"
+    },
+    {
+        'author':'Vignesh',
+        'title': "Second Blog Post",
+        'content': "Content of second blog post",
+        'date_posted': "13-Nov-2020"
+    }
+]
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return render_template("home.html",posts=posts)
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
-if "__name__"=="__main":
+if __name__=="__main__":
     app.run(debug=True)
